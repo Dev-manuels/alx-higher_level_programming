3479026162de426f80c1c9895843c456579934b3
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-06-07 15:18:54 +0100

    handle negative number outpuut bug

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print(f"Last digit of {number} is {abs(number) % 10}", end="")
-if abs(number) % 10 > 5:
+outnumb = abs(number) % 10
+if number < 0:
+    outnumb = (abs(number) % 10) * -1
+print(f"Last digit of {number} is {outnumb}", end="")
+if outnumb > 5:
     print(f" and is greater than 5")
-elif abs(number) % 10 < 6 and abs(number) % 10 != 0:
+elif outnumb < 6 and outnumb != 0:
     print(f" and is less than 6 and not 0")
 else:
     print(f" and is 0")
