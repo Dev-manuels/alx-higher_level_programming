8f782493a7b1ff87c14d6e651baa609ec13fe11f
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-06-12 17:40:26 +0100

    test task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for row in matrix:
+            for item in row:
+                if item == row[-1]:
+                    print("{:d}".format(item))
+                else:
+                    print("{:d}".format(item), end=" ")
