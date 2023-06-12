3e82ad72c69de110d285e15a6070896660d75d1b
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-06-12 17:55:22 +0100

    correct output format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,7 @@ def print_matrix_integer(matrix=[[]]):
         for row in matrix:
             for item in row:
                 if item == row[-1]:
-                    print("{:d}".format(item))
+                    print("{:d}".format(item), end="")
                 else:
                     print("{:d}".format(item), end=" ")
+            print()
