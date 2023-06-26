272f66b3383a83687336741da7559e3a5bb04711
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-06-26 20:45:54 +0100

    handle expected exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,13 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
-            print(my_list[i], end=" ")
+            print(f"{my_list[i]}", end=" ")
         print()
+        return (i)
     except IndexError:
-        pass
+        count = 0
+        for i in my_list:
+            print(f"{i}", end=" ")
+            count += 1
+        print()
+        return (count)
