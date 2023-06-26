9c2c054c6161f659d2720cc1f913021ae56fbec3
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-06-26 21:20:20 +0100

    catch all errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,6 @@ def safe_print_list(my_list=[], x=0):
             print(f"{my_list[i]}", end=" ")
         print("")
         return (i)
-    except IndexError:
+    except Exception:
         print("")
         return (i)
