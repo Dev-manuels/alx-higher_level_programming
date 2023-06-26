5f4727d682ef60ec27cea67f60f8f102e97679dd
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-06-26 21:41:19 +0100

    return on success or failure

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,5 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except Exception:
+    except (TypeError, ValueError):
+        pass
+    finally:
         return (isinstance(value, int))
