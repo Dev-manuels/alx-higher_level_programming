45aea6f1a0e8b0ffd347ad4367569b382966fe51
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-06-26 22:45:25 +0100

    handle any exception at all

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 count += 1
             except (TypeError, ValueError):
                 i -= 1
-    except IndexError:
+    except Exception:
         pass
     finally:
         print("")
