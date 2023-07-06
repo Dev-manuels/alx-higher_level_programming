03b139f074fc3cb5a075123f17f88ebf6b8163c9
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-07-06 03:33:28 +0100

    improve code

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,8 +19,8 @@ class Rectangle:
         height (value): Setter to set the height of the rectangle
     """
     def __init__(self, width=0, height=0):
-        self._width = width
-        self._height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
