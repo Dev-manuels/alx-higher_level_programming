b1541b62d578ba74d419820c22e143ec06d46ae9
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-07-06 04:02:06 +0100

    correct task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -50,4 +50,6 @@ class Rectangle:
         return (self.width * self.height)
 
     def perimeter(self):
+        if self.area == 0:
+            return (0)
         return (2 * (self.width + self.height))
