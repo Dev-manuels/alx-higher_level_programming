5afede0f22da9a229c9c606b3351804654905b6e
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-09 17:56:32 +0100

    fix logical bug

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+j = 0
 for i in range(122, 96, -1):
-    if i % 2 == 0:
+    j += 1
+    if j % 2 == 1:
         print(chr(i), end="")
     else:
         print(chr(i - 32), end="")
