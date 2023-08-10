03e29087e959a951b9721089194c2e06581e21c0
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-10 22:22:21 +0100

    output using string format

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,6 +3,6 @@ j = 0
 for i in range(122, 96, -1):
     j += 1
     if j % 2 == 1:
-        print(chr(i), end="")
+        print("{}".format(chr(i)), end="")
     else:
-        print(chr(i - 32), end="")
+        print("{}".format(chr(i - 32)), end="")
