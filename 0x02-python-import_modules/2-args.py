7eaa97cc22bd250769407e4f7eb77d6140ae30de
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-11 23:50:51 +0100

    attempt taask 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
-if __name == "__main__":
-    
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) == 1:
+        print("{} argument:".format(len(sys.argv)))
+    elif len(sys.argv) == 0:
+        print("{} arguments.".format(len(sys.argv))) 
+    else:
+        print("{} arguments:".format(len(sys.argv)))
+    i = 1
+    for arg in sys.argv:
+        print("{}: {}".format(i, arg))
+        i += 1
