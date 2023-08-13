1b79f609673a9b943cd459e39d295328b2b83ac3
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-13 08:42:26 +0100

    attempt task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle - function that checks for a cycle in a linked list
+ * using Floyd’s Cycle Finding Algorithm
+ * @list: pointer to start of the list
+ * Return: 0 if there is no cycle, 1 if there is
+*/
+int check_cycle(listint_t *list)
+{
+    listint_t *tortoise, *hare;
+
+    if (list == NULL)
+        return (0);
+
+    tortoise = list;
+    hare = list;
+    while (hare != NULL && hare->next->next != NULL)
+    {
+        tortoise = tortoise->next;
+        hare = hare->next->next;
+        if (tortoise == hare)
+            return (1);
+    }
+    return (0);
+}
