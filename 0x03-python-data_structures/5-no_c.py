379776759d322a05a4c19c0271c1e0cd9a6d1df9
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-16 11:37:23 +0100

    handle when string is empty

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if len(my_string) > 0:
-        new_string = ""
+    new_string = ""
+    if my_string is not None and len(my_string) > 0:
         for letter in my_string:
             if letter != "c" and letter != "C":
                 new_string += letter
         return new_string
+    return new_string
