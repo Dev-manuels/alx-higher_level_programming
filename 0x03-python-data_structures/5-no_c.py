c627d7072cd02ebc8971379204ba2d91c27f5717
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-16 11:26:33 +0100

    attempt task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if len(my_string) > 0:
+        new_string = ""
+        for letter in my_string:
+            if letter != "c" and letter != "C":
+                new_string.join(letter)
+        return new_string
