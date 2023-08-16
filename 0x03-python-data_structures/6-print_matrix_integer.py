659b556c148956ed23cc023bc60016b3572e9f26
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-16 12:12:56 +0100

    swap positions

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is not None and len(matrix) > 0:
+    if len(matrix) > 0 and matrix is not None:
         for row in matrix:
             for column in row:
                 if column != row[-1]:
