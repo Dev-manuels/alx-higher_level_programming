fa1a80ad6c05d2c32c3b244338b4d7b4be7f4522
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-16 11:51:55 +0100

    attempt task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix is not None and len(matrix) > 0:
+        for row in matrix:
+            for column in row:
+                if column != row[-1]:
+                    print("{}".format(column), end=" ")
+                else:
+                    print("{}".format(column))
