3533af2925ed2f42ce72ae40cc19265635e34568
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-16 11:32:46 +0100

    correct task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,5 +4,5 @@ def no_c(my_string):
         new_string = ""
         for letter in my_string:
             if letter != "c" and letter != "C":
-                new_string.join(letter)
+                new_string += letter
         return new_string
