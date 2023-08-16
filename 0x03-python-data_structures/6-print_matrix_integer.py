88d64ddfbde816790b3f54eb331bf77fb4fe4247
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-16 15:48:21 +0100

    change logical operator to OR

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) > 0 and matrix is not None:
+    if matrix == [[]] or matrix is None:
+        print()
+    else:
         for row in matrix:
             for column in row:
                 if column != row[-1]:
                     print("{:d}".format(column), end=" ")
                 else:
                     print("{:d}".format(column))
-    else:
-        print("")
