b1942b672dc95504949170724666fafb1c9ad280
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-16 10:16:57 +0100

    correct arg to dir

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    for name in dir(hidden_4.pyc):
+    import hidden_4
+    
+    for name in dir(hidden_4):
         if name[0] != "_":
             print(name)
