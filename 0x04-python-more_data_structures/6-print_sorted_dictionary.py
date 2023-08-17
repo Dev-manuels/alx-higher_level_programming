ed6080e6e8659e4f30ee62b23c4daef621757540
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-17 23:32:51 +0100

    squash bugs

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     if a_dictionary is not None:
-        keys = sorted(a_dictionary)
-        for key in keys:
+        keys = a_dictionary.keys()
+        for key in sorted(keys):
             print(key, ":", a_dictionary[key])
--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary is not None:
+    if a_dictionary is not None and len(a_dictionary) > 0:
         tmp = dict([(key, value)])
         a_dictionary.update(tmp)
     return a_dictionary
