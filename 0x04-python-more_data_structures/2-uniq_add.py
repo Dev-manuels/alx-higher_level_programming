20e313674eae7979edf3ff094014b3245d6600ba
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-17 21:45:05 +0100

    add whitespace

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     if len(my_list) >= 0:
-        uniq= set(my_list)
+        uniq = set(my_list)
         sum = 0
         for item in uniq:
             sum += item
