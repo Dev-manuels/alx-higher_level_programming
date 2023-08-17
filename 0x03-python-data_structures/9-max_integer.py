2e0e3502e460a9c652fed023135850fb67778632
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-17 12:33:05 +0100

    add support for negative numbers

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) > 0:
-        large = 0
+        large = -5
         for number in my_list:
             if number >= large:
                 large = number
