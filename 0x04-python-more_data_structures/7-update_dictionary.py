dcfe6a1d22e4b5dec972f63b9c710a964a90f963
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-18 20:05:41 +0100

    simplify code

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     if a_dictionary is not None and len(a_dictionary) > 0:
-        tmp = dict([(key, value)])
-        a_dictionary.update(tmp)
+        a_dictionary.update({key: value})
     return a_dictionary
