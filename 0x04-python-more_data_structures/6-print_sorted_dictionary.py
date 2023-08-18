7664be93cc2c083c945111b93d3276728c04f6f7
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-18 18:19:27 +0100

    improve logic

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    if a_dictionary is not None:
+    if a_dictionary is not None and len(a_dictionary) > 0:
         keys = a_dictionary.keys()
         for key in sorted(keys):
             print(key, ":", a_dictionary[key])
