fbee96a87790c914222408b9326aa97e7f106122
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-18 18:24:20 +0100

    use string format to print

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@ def print_sorted_dictionary(a_dictionary):
     if a_dictionary is not None and len(a_dictionary) > 0:
         keys = a_dictionary.keys()
         for key in sorted(keys):
-            print(key, ":", a_dictionary[key])
+            print("{} : {}".format(key, a_dictionary[key]))
