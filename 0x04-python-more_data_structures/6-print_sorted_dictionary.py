c2603c1fe72009d8fb6ee979d6118105efad565e
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-18 22:19:20 +0100

    batch fix

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    if len(a_dictionary) > 0:
-        for key, value in sorted(a_dictionary.items()):
-            print("{} : {}".format(key, value))
+    for key in sorted(a_dictionary.keys()):
+        print("{} : {}".format(key, value))
--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary is not None and key in a_dictionary:
-        new_dict = dict()
-        for keys, values in a_dictionary.items():
-            if keys == key:
-                new_dict.update({keys: value})
-            else:
-                new_dict.update({keys: values})
-        return new_dict
+    a_dictionary[key] = value
     return a_dictionary
