7ad46f208605ceeecf7f0b065f06f6a80cea15e2
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-18 22:00:20 +0100

    modify if statement

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary is not None and len(a_dictionary) > 0:
-        a_dictionary.update({key: value})
+    if a_dictionary is not None and key in a_dictionary:
+        new_dict = dict()
+        for keys, values in a_dictionary.items():
+            if keys == key:
+                new_dict.update({keys: value})
+            else:
+                new_dict.update({keys: values})
+        return new_dict
     return a_dictionary
