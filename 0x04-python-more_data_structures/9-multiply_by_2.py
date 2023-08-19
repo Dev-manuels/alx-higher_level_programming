1ca4908e7ecb18ad247d4a45bae55e6a67cb2a11
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-19 08:46:12 +0100

    attempt task 9

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    if a_dictionary is not None:
+        new_dict = dict()
+        for key, value in a_dictionary.items():
+            new_dict.update({key: (value * 2)})
+        return new_dict
