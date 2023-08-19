253612a1bc742ea6bb5c1622a1445e042b179459
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-19 08:40:17 +0100

    handle empty dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if len(a_dictionary) > 0:
+    if a_dictionary is not None:
         a_dictionary.update({key: value})
     return a_dictionary
