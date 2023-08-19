94190f31cd23a0b0a42de03c29dc1237deab22b7
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-19 08:55:40 +0100

    attempt task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is not None:
+        large = -50
+        for value in a_dictionary.values():
+            if value >= large:
+                large = value
+        return large
