16b18de089c57e4e9a397817490212b7dbc10f21
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-19 20:56:09 +0100

    improve handling for empty list

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if a_dictionary and len(a_dictionary) > 0:
+    if a_dictionary is not None and len(a_dictionary) > 0:
         keys = []
         for key, val in a_dictionary.items():
             if val == value:
