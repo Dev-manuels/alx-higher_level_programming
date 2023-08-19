6064e2aac325d8e3be598151406ce8693a1a7cd5
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-19 20:53:18 +0100

    Handle empty list

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if a_dictionary:
+    if a_dictionary and len(a_dictionary) > 0:
+        keys = []
         for key, val in a_dictionary.items():
             if val == value:
-                del (a_dictionary[key])
+                keys.append(key)
+        for key in keys:
+            del (a_dictionary[key])
         return a_dictionary
