70db8503dd990f204305c6f54484520b86fdf224
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-19 09:20:36 +0100

    fix bugs

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if len(a_dictionary) > 0:
         large = 0
         b_key = ""
         for key, value in a_dictionary.items():
