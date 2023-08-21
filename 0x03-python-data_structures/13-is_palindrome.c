e58e4b677341ef0cbf56739a9591f93005466154
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-21 14:57:00 +0100

    Attempt palindrome task

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,74 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * reverse_list - Function that reverses a list to a defined end point
+ * @head: pointer to the start of the list
+ * @ptr_end: pointer to desired end of reversed list. Can be NULL.
+ * Return: pointer to new list on success else Null on failure
+*/
+listint_t *reverse_list(listint_t **head, listint_t *ptr_end)
+{
+	/* Check that head is valid */
+	if (head == NULL || *head == NULL || (*head)->next == NULL)
+	{
+		return (NULL);
+	}
+
+	listint_t *current = *head, *prev = NULL, *next;
+
+	/* Check if a desired node end is set */
+	if (ptr_end != NULL)
+		prev = ptr_end;
+	/* Reverse the list */
+	while (current->next != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	return (current);
+}
+
+/**
+ * is_palindrome - Function that checks if a singly list is a palindrome
+ * @head: pointer to start of list
+ * Return: 0 if it is not a palindrome and 1 if it is.
+*/
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL || *head == NULL || (*head)->next == NULL)
+		return (0);
+
+	listint_t *slow = *head, *fast = *head, *midpoint;
+
+	while (fast->next != NULL && fast->next->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	if (fast->next == NULL)
+	{
+		midpoint = slow;
+		while (fast->next != midpoint)
+			fast = fast->next;
+		fast = reverse_list(&fast, NULL);
+		slow = slow->next;
+	} else
+	{
+		midpoint = slow->next;
+		while (fast->next != midpoint)
+			fast = fast->next;
+		fast = reverse_list(&fast, NULL);
+		slow = slow->next;
+	}
+	while (slow != NULL && fast != NULL)
+	{
+		if (fast->n != slow->n)
+			return (0);
+		slow = slow->next;
+		fast = fast->next;
+	}
+	return (1);
+}
