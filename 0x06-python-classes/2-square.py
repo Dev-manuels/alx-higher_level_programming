d69789b3ab7ce166038990c9b31c3da8e1923bb8
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-29 19:18:52 +0100

    update logic

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,9 +12,9 @@ class Square:
         Args:
             __size (int): size of square object
         """
-        if isinstance(int, size):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif int(size) < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = int(size)
