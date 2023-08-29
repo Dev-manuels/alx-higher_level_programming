525b46ffe2428e707f3da999349b5b7d63af2c10
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-29 19:38:42 +0100

    attempt task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Module containing a Square Class """
+
+
+class Square:
+    """Simple Square class
+    """
+
+    def __init__(self, size=0):
+        """Initialize Square object
+
+        Args:
+            __size (int): size of square object
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif int(size) < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(size)
+
+    def area(self):
+        """Calculates the area of a Square object
+
+        Returns:
+            int: area of the Square object
+        """
+        return (self.__size * self.__size)
