73c335fdcfe9bb8d9f9acce7a6310db916a7488d
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-09-06 07:10:27 +0100

    Add README and fix bugs
    passed all tests for task 0 and 1
    add execute permission to both modules

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+Module for matrix_divided
+"""
+
+
+def matrix_divided(matrix, numb):
+    """matrix_divided divides a matrix by div
+
+    Args:
+        matrix (list): list of list with values to be divided
+        numb (int): divisor
+
+    Raises:
+        TypeError: When matrix is not a list of list containing int/floats,
+        When matrix is not a list, when matrix is not a list containing
+        list of equal lengths
+        ZeroDivisionError: when divisor is Zero
+
+    Returns:
+        list: new list with all elements divided by divisor
+    """
+    new_list = []
+    """Validate div"""
+    if not isinstance(numb, (int, float)):
+        raise TypeError(
+            "div must be a number"
+        )
+    elif numb == 0:
+        raise ZeroDivisionError("division by zero")
+    """Validate matrix"""
+    if not isinstance(matrix, list):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of "
+            "integers/floats"
+        )
+    """
+    check that matrix is a list of list
+    and containing floats or int
+    """
+    for row in matrix:
+        if isinstance(row, list):
+            if len(row) != len(matrix[0]):
+                raise TypeError(
+                    "Each row of the matrix must have the same size"
+                )
+            for item in row:
+                if not isinstance(item, (int, float)):
+                    raise TypeError(
+                        "matrix must be a matrix (list of lists) of "
+                        "integers/floats"
+                    )
+        else:
+            raise TypeError(
+                "matrix must be a matrix (list of lists) "
+                "of integers/floats"
+            )
+    """perform matrix division"""
+    for row in matrix:
+        new_row = []
+        for item in row:
+            new_row.append(round((item / numb), 2))
+        new_list.append(new_row)
+    return new_list
