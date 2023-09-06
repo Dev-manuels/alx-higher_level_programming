3e57c0295ba4d633afe390969982c7fd8b2b4263
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-09-06 17:35:28 +0100

    handle no arg
    Set size to default to None when no argument is passed

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -4,17 +4,18 @@ Module containing print_square
 """
 
 
-def print_square(size):
-    """print_square prints a square to screen
+def print_square(size=None):
+    """print_square _summary_
 
     Args:
-        size (int): size length of the square
+        size (int, optional): Size of square.
+        Defaults to None and prints nothing.
 
     Raises:
         TypeError: If size is not an integer
         ValueError: when size is less than zero
     """
-    if not isinstance(size, int):
+    if not isinstance(size, int) or size is None:
         raise TypeError("size must be an integer")
     elif size < 0:
         raise ValueError("size must be >= 0")
