52910aecec2fc351d2aa9e87daa244e2e73ddea8
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-09-13 09:22:44 +0100

    Add function documentation

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,4 +6,13 @@ checks that an object and a class are same
 
 
 def is_same_class(obj, a_class):
+    """is_same_class checks that an object and a class are same
+
+    Args:
+        obj (object)
+        a_class (class):
+
+    Returns:
+        Boolean: True or False
+    """
     return (type(obj) is a_class)
