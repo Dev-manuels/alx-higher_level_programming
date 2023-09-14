7194736c40d74db44f1d86ce3cd88f167289e753
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-09-14 10:41:43 +0100

    Attemot task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module containing Class Rectangle that inherits from
+Class BaseGeometry in module "7-base_geometry.py"
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle
+    inherits from class BaseGeometry
+
+    Args:
+        BaseGeometry (class): Base class for class Rectangle
+    """
+    def __init__(self, width, height):
+        """__init__
+        constructor for class Rectangle
+
+        Args:
+            width (int): width of rectangle object to be created
+            height (int): height of rectangle object to be created
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return (self.__height * self.__width)
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
