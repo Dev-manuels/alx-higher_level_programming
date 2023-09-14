91e0d35c43a1f236e5327d88c5c4e01ece601e54
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-09-14 10:49:31 +0100

    Attempt task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module containing class Square
+that inherits from class Rectangle
+in module (9-rectangle.py)
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Square
+    Inherits from class Rectangle
+
+    Args:
+        Rectangle (Class): Parent class
+    """
+    def __init__(self, size):
+        """__init__
+        Class Rectangle constructor
+
+        Args:
+            size (int): size of Square object
+        """
+        self.integer_validator(size)
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
