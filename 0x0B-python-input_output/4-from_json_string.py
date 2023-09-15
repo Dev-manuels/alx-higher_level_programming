cea6f6e2bc419585a3ed8f1f900ba6f1720e90c4
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-09-15 23:17:10 +0100

    add function documentation

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -7,4 +7,13 @@ import json
 
 
 def from_json_string(my_str):
+    """from_json_string
+    returns  (Python data structure) represented by a JSON string
+
+    Args:
+        my_str (JSON string): JSON string to be converted to python object
+
+    Returns:
+        python object: decoded JSON string
+    """
     return json.loads(my_str)
