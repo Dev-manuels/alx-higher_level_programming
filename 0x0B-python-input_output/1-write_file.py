9af6062f197b03860d6ae3fd6d470d5bb3742e04
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-09-15 22:30:45 +0100

    attempt task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module containing a function that writes to a file
+"""
+
+
+def write_file(filename="", text=""):
+    """write_file writes text to a file
+
+    Args:
+        filename (str): file path/ file name. Defaults to "".
+        text (str): text to write to file. Defaults to "".
+    """
+    if len(filename) > 1 and len(text) > 1:
+        with open(filename, mode="w", encoding="utf-8") as file:
+            count = file.write(text)
+    return count
