9c1ee8a5344da4b1100f59f719be453e03e394e4
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-09-15 23:06:25 +0100

    add function documentation

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -7,4 +7,13 @@ import json
 
 
 def to_json_string(my_obj):
+    """to_json_string
+    returns the JSON representation of an object
+
+    Args:
+        my_obj (object): object to be converted to JSON
+
+    Returns:
+        JSON: JSON representation object
+    """
     return (json.dumps(my_obj))
