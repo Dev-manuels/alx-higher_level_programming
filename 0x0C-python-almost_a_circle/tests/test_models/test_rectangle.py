2cf88435ee9d5f95cf3baa1fe9ac78e999fc62d6
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-09-20 22:20:07 +0100

    add documentation and simpllify test for str
    used a more simple test for the string representation of an object

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -56,59 +56,83 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(rect.y, 10)
 
     def test_width_getter(self):
+        """test_width_getter
+        """
         r = Rectangle(10, 20)
         self.assertEqual(r.width, 10)
 
     def test_width_setter(self):
+        """test_width_setter
+        """
         r = Rectangle(10, 20)
         r.width = 30
         self.assertEqual(r.width, 30)
 
     def test_width_setter_type_error(self):
+        """test_width_setter_type_error
+        """
         r = Rectangle(10, 20)
         with self.assertRaises(TypeError):
             r.width = "invalid"
 
     def test_height_setter_type_error(self):
+        """test_height_setter_type_error
+        """
         r = Rectangle(10, 20)
         with self.assertRaises(TypeError):
             r.height = "invalid"
 
     def test_x_setter_type_error(self):
+        """test_x_setter_type_error
+        """
         r = Rectangle(10, 20)
         with self.assertRaises(TypeError):
             r.x = "invalid"
 
     def test_y_setter_type_error(self):
+        """test_y_setter_type_error
+        """
         r = Rectangle(10, 20)
         with self.assertRaises(TypeError):
             r.y = "invalid"
 
     def test_width_setter_value_error(self):
+        """test_width_setter_value_error
+        """
         r = Rectangle(10, 20)
         with self.assertRaises(ValueError):
             r.width = 0
 
     def test_height_setter_value_error(self):
+        """test_height_setter_value_error
+        """
         r = Rectangle(10, 20)
         with self.assertRaises(ValueError):
             r.height = -1
 
     def test_x_setter_value_error(self):
+        """test_x_setter_value_error
+        """
         r = Rectangle(10, 20)
         with self.assertRaises(ValueError):
             r.x = -5
 
     def test_y_setter_value_error(self):
+        """test_y_setter_value_error
+        """
         r = Rectangle(10, 20)
         with self.assertRaises(ValueError):
             r.y = -2
 
     def test_area(self):
+        """test_area
+        """
         r = Rectangle(10, 2)
         self.assertEqual(r.area(), 20)
 
     def test_display(self):
+        """test_display
+        """
         # Redirect stdout to capture the printed output
         with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
             self.r1.display()
@@ -121,18 +145,15 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(output, expected_output)
 
     def test_str(self):
-        # Redirect stdout to capture the printed output
-        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
-            print(self.r1)
-
-            # Get the captured output
-            output = mock_stdout.getvalue()
-
-        # Verify that the displayed rectangle matches the expected output
-        expected_output = "[Rectangle] (34) 0/0 - 3/2\n"
-        self.assertEqual(output, expected_output)
+        """test_str
+        """
+        r1 = Rectangle(3, 2, 0, 0, 34)
+        expected_output = "[Rectangle] (34) 0/0 - 3/2"
+        self.assertEqual(str(r1), expected_output)
 
     def test_update_args(self):
+        """test_update_args
+        """
         rect = Rectangle(1, 2)
         self.assertEqual(rect.width, 1)
         self.assertEqual(rect.height, 2)
@@ -144,6 +165,8 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(rect.id, 30)
 
     def test_update_kwargs(self):
+        """test_update_kwargs
+        """
         rect2 = Rectangle(1, 2)
         rect2.update(id=30, width=40, height=5, x=10, y=60)
         self.assertEqual(rect2.width, 40)
