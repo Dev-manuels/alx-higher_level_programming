6ca65aa141870bf6e6fb6596e2aec142dc9512dc
Dev Manuels <Emmanuel.umeile@gmail.com>
2024-02-17 16:48:10 +0100

    complete task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module containing script that lists all states
+with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    # Create db Connection
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    # Create a cursor object
+    cur = db.cursor()
+    # execute select query
+    cur.execute("SELECT * FROM states WHERE state.name REGEXP '%^N%' ORDER BY states.id")
+    rows = cur.fetchall()
+    # display result of select query
+    for row in rows:
+        print(row)
+
+    # Clean up actions
+    cur.close()
+    db.close()
