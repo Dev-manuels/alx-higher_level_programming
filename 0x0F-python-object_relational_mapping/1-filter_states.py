8c5f8ea3a187d7dd89dba6f97cbf20b3ef6d34b7
Dev Manuels <Emmanuel.umeile@gmail.com>
2024-02-17 17:11:59 +0100

    revert query

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     # Create a cursor object
     cur = db.cursor()
     # execute select query
-    cur.execute("SELECT * FROM states WHERE states.name \
-                REGEXP '^N%' ORDER BY states.id")
+    cur.execute("SELECT * FROM states WHERE name \
+                REGEXP '^N%' ORDER BY id")
     rows = cur.fetchall()
     # display result of select query
     for row in rows:
