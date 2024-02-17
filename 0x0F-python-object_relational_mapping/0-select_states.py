d5a676a4fa71dd3c4720f08e9f866fe07cf8c399
Dev Manuels <Emmanuel.umeile@gmail.com>
2024-02-17 16:07:42 +0100

    add relevant comments

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,9 +8,17 @@ import sys
 
 
 if __name__ == "__main__":
+    # Create db Connection
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    # Create a cursor object
     cur = db.cursor()
+    # execute select query
     cur.execute("SELECT * FROM states ORDER BY states.id")
     rows = cur.fetchall()
+    # display result of select query
     for row in rows:
         print(row)
+
+    # Clean up actions
+    cur.close()
+    db.close()
