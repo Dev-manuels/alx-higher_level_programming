70f47be51ae8c9c6f400ad15ac3df613f944eedd
Dev Manuels <Emmanuel.umeile@gmail.com>
2024-02-19 13:30:38 +0100

    fix SQL query

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     # execute select query
     cur.execute("SELECT * FROM states WHERE name \
-                REGEXP '^N%' ORDER BY id")
+                LIKE 'N%' ORDER BY id")
     rows = cur.fetchall()
     # display result of select query
     for row in rows:
