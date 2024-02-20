0ffef93c07922d9cf9870ace1e186265c17d5ef2
Dev Manuels <Emmanuel.umeile@gmail.com>
2024-02-20 17:18:42 +0100

    modify select statement

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    query = "SELECT * FROM cities ORDER BY cities.id ASC"
+    query = "SELECT id, name FROM cities ORDER BY cities.id ASC"
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
