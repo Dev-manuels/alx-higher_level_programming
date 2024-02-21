cc3c5d048c6e41bf14bde2a30e1be26deb2c98d3
Dev Manuels <Emmanuel.umeile@gmail.com>
2024-02-21 16:39:28 +0100

    remove comments and ensure pycodestyle compliance

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,23 +2,6 @@
 """
 Module containing the class definition of a State
 and an instance Base = declarative_base():
-
-CREATE DATABASE IF NOT EXISTS hbtn_0e_4_usa;
-USE hbtn_0e_4_usa;
-CREATE TABLE IF NOT EXISTS states ( 
-    id INT NOT NULL AUTO_INCREMENT, 
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
-
-CREATE TABLE IF NOT EXISTS cities ( 
-    id INT NOT NULL AUTO_INCREMENT, 
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY(state_id) REFERENCES states(id)
-);
 """
 import sys
 from sqlalchemy import Column, Integer, String
@@ -27,7 +10,14 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
+    """
+    State - contains the class definition of a State
+
+    Args:
+        Base (SuperClass): declarative base of sqlalchemy
+    """
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
