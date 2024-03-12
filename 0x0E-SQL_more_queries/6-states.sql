a049d45e70d4b8a291fc0bf2f11d242b00a0c9d0
Dev Manuels <Emmanuel.umeile@gmail.com>
2024-03-12 17:46:37 +0100

    specify db for table creation

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,7 +2,7 @@
 -- id INT unique, auto generated, can’t be null and is a primary key
 -- name VARCHAR(256)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
     id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
