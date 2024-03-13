677c0840a08505c0ae0114138605e4e451cd2d92
Dev Manuels <Emmanuel.umeile@gmail.com>
2024-03-13 14:58:30 +0100

    update comments

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,4 @@
--- script that lists all shows contained in hbtn_0d_tvshows that have at least
+-- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
 SELECT DISTINCT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows FROM tv_show_genres
  INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id 
  INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id  
--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,4 @@
--- script that lists all shows contained in hbtn_0d_tvshows that have at least
+-- script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
 SELECT DISTINCT tv_genres.name FROM tv_show_genres
  INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
  INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
